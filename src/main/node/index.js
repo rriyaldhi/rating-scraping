374c4daa6b5640a07216dec7fb2d79a955719c63
Rizky Riyaldhi <rizky.riyaldhi@getyourguide.com>
2024-02-15 15:46:45 +0100

    Comment captcha solver and screenshot

--- src/main/node/index.js
+++ src/main/node/index.js
@@ -13,17 +13,17 @@ async function main() {
         console.log('Connected! Navigating...');
         const page = await browser.newPage();
         await page.goto('https://www.tripadvisor.com/AttractionProductReview-g255060-d11458687-Day_Trip_with_Chef_Led_Hunter_Valley_Gourmet_Food_and_Wine_from_Sydney-Sydney_New_.html', { timeout: 2 * 60 * 1000 });
-        console.log('Taking screenshot to page.png');
-        await page.screenshot({ path: './page.png', fullPage: true });
+        // console.log('Taking screenshot to page.png');
+        // await page.screenshot({ path: './page.png', fullPage: true });
         console.log('Navigated! Scraping page content...');
         const html = await page.content();
-        console.log(html);
+        // console.log(html);
         // CAPTCHA solving: If you know you are likely to encounter a CAPTCHA on your target page, add the following few lines of code to get the status of Scraping Browser's automatic CAPTCHA solver
         // Note 1: If no captcha was found it will return not_detected status after detectTimeout
         // Note 2: Once a CAPTCHA is solved, if there is a form to submit, it will be submitted by default
-        const client = await page.target().createCDPSession();
-        const {status} = await client.send('Captcha.solve', {detectTimeout: 30*1000});
-        console.log(`Captcha solve status: ${status}`)
+        // const client = await page.target().createCDPSession();
+        // const {status} = await client.send('Captcha.solve', {detectTimeout: 30*1000});
+        // console.log(`Captcha solve status: ${status}`)
         const $ = cheerio.load(html);
         const text = $('[data-automation="reviewBubbleScore"]').text();
         console.log(text);
